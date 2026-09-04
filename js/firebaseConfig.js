107c6566bdd77cb19f54997b86eda8c940e9f578
gonzaasuarez3-ctrl <gonzaasuarez3@gmail.com>
2026-09-04 22:14:03 +0200

    ..aa

--- js/firebaseConfig.js
+++
@@ -1,8 +0,0 @@
-export const firebaseConfig = {
-  apiKey: "AIzaSyD2DJI2b5byrkiO3QAgc2gi1iPuki-nl0c",
-  authDomain: "yard-guardian-3dd6f.firebaseapp.com",
-  projectId: "yard-guardian-3dd6f",
-  storageBucket: "yard-guardian-3dd6f.firebasestorage.app",
-  messagingSenderId: "951150403109",
-  appId: "1:951150403109:web:e2dd3aa81e8f7b1c5c69cc"
-};
